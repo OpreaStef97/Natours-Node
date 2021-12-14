7b73474448a5947b6eaf466a20b626339244a62f
Oprea Stefan <opreastef97@gmail.com>
2021-12-14 15:42:40 +0200

    Improved stripe implementation 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         success_url: `${req.protocol}://${req.get('host')}/my-tours`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
-        client_reference_id: req.params.tourId,
+        client_reference_id: req.params.tourID,
         mode: 'payment',
         line_items: [
             {
